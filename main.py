d56a173dddaecbfd5faf9d25885bbd46735242d7
Safestt <126298996+Safestt@users.noreply.github.com>
2025-02-26 21:45:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
-from pathlib import Path
+import flet as ft
+import os
 
-app = FastAPI()
-active_connections = []  # Lista de conexiones activas
+def main(page: ft.Page):
+    page.title = "Prueba de Deploy en Render"
+    page.add(ft.Text("¡Hola, Render! 🎉"))
 
-@app.get("/")
-async def get():
-    return HTMLResponse(content=Path("index.html").read_text(encoding="utf-8"))
-
-@app.websocket("/ws")
-async def websocket_connection(websocket: WebSocket):
-    await websocket.accept()
-    active_connections.append(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            for connection in active_connections:
-                if connection != websocket:  # Evita enviar el mensaje de vuelta al remitente
-                    await connection.send_text(f"Anonimo: {data}")
-    except:
-        print("❌ Cliente desconectado")
-    finally:
-        active_connections.remove(websocket)
+ft.app(target=main, view=ft.WEB_BROWSER, port=int(os.getenv("PORT", 8000)))  
